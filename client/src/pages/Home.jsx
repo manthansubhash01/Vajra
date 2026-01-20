96dc632d12209e6787acf445c10c6e068178e9f6
manthansubhash01 <manthanziman20@gmail.com>
2026-01-20 22:09:02 +0530

    Home page and Hero section implementation

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React,{useState,useEffect} from 'react';
+import HeroSection from '../components/HeroSection';
+
+const Home = () => {
+  return (
+    <div className='relative w-full h-screen'>
+        <HeroSection/>
+    </div>
+  )
+}
+
+export default Home
