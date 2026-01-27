4efc860923fc8e2021b0ab922f442d5df2aeccc6
manthansubhash01 <manthanziman20@gmail.com>
2026-01-27 21:29:54 +0530

    updated user model

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,39 +1,58 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  age: {
-    type: Number,
-    required: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  wishlist: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Product",
+    name: {
+        type: String,
+        required: true,
     },
-  ],
+    password: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    wishlist: [
+        {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        },
+    ],
+    cart: [
+        {
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product",
+            required: true,
+        },
+        variantSku: {
+            type: String,
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            min: 1,
+            default: 1,
+        },
+        },
+    ],
 });
 
 module.exports = mongoose.model("User", userSchema);
