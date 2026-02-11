190836c63609e9f18453ba5e6590865a99c4c144
manthansubhash01 <manthanziman20@gmail.com>
2026-02-11 22:32:26 +0530

    added order routes and controller

---
+++ server/src/routes/order.routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const { verifyToken } = require("../middlewares/authentication.middleware");
+const {
+    getUserOrders,
+    getOrderById,
+    createOrder,
+    cancelOrder,
+} = require("../controllers/order.controller");
+
+router.use(verifyToken);
+
+router.post("/", createOrder);
+router.get("/", getUserOrders);
+router.get("/:orderId", getOrderById);
+router.patch("/:orderId/cancel", cancelOrder);
+
+module.exports = router;
